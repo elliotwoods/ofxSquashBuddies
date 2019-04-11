c4ebcb66069fe8f49cc65d963f9bccfec3d2351c
Elliot Woods <elliot@kimchiandchips.com>
2019-04-11 21:34:12 +0900

    0.10.0 fixes

--- src/ofxSquashBuddies/Utils.cpp
+++ src/ofxSquashBuddies/Utils.cpp
@@ -1,5 +1,7 @@
 #include "Utils.h"
 
+using namespace std;
+
 namespace ofxSquashBuddies {
 	namespace Utils {
 		//----------
--- src/ofxSquashBuddies/Utils.h
+++ src/ofxSquashBuddies/Utils.h
@@ -8,10 +8,10 @@ namespace ofxSquashBuddies {
 		class FramerateCounter {
 		public:
 			void update();
-			void addFrame(chrono::high_resolution_clock::time_point = chrono::high_resolution_clock::now());
+			void addFrame(std::chrono::high_resolution_clock::time_point = std::chrono::high_resolution_clock::now());
 			float getFrameRate() const;
 		protected:
-			queue<chrono::high_resolution_clock::time_point> frameTimes;
+			std::queue<std::chrono::high_resolution_clock::time_point> frameTimes;
 			float frameRate = 0.0f;
 		};
 	}
